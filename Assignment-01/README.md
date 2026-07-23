b0731240801cf818570c670e37c83c851ae1afa5
FI-Nabil <finabil98@gmail.com>
2026-07-23 12:52:15 +0600

    Add configurable thresholds and warning alerts to health check script

--- Assignment-01/README.md
+++ Assignment-01/README.md
@@ -8,3 +8,6 @@
 * `uptime` --> It displays how long the system is running and the cpu load over the last 1,5,15 minutes
 * `ps -eo pid,user,%mem,command --sort=-%mem` --> it shows every process (-e), formats the columns(-0) and sort them by top memory consumption
 * `| head -n 6` --> | is pipe, it catches the output of the previous command, head -n 6 takes the output from the top and takes first 6 rows, first row is the column header.
+* `while [ $# -gt 0 ]` --> it loops over the commnad-line arguments until anything left. 
+* `shift` --> shift remove the first argument and shift the index of the remaining to the left.
+* `awk` --> a command line used for text processing.
--- Assignment-01/health-check.sh
+++ Assignment-01/health-check.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+threshold=0
 logfile="health-check.log"
+
+while [ $# -gt 0 ]
+do
+    if [ "$1" = "--threshold" ]; then
+        threshold="$2"
+        shift 2
+
+    elif [ "$1" = "--log" ]; then
+        logfile="$2"
+        shift 2
+
+    else
+        echo "Unknown option: $1"
+        echo "Usage: ./health-check.sh --threshold <value> --log <logfile>"
+        exit 1
+    fi
+done
 
 {
   echo "==="
@@ -9,10 +27,25 @@ logfile="health-check.log"
 
   echo "==Disk Usage=="
   df -h /
+  disk_usage=$(df -h / | awk 'NR==2 {gsub("%","",$5); print $5}')
+
+  if [ "$disk_usage" -gt "$threshold" ]; then
+        echo "WARNING: Disk usage above ${threshold}%! Current: ${disk_usage}%"
+    else
+        echo "Disk usage is within threshold."
+    fi
+
   echo ""
 
   echo "==Memory Usage=="
   free -h
+   memory_usage=$(free | awk '/Mem:/ {printf "%.0f", $3/$2 * 100}')
+
+    if [ "$memory_usage" -gt "$threshold" ]; then
+        echo "WARNING: Memory usage above ${threshold}%! Current: ${memory_usage}%"
+    else
+        echo "Memory usage is within threshold."
+    fi
   echo ""
 
   echo "==CPU Load=="
